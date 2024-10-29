648e604130d7fc7c8359903f0fa493c71f967391
kaikyou76 <kaikyou@gmail.com>
2024-10-29 10:59:18 +0800

    【Drizzle ORM】NextJs14 と Drizzle ORM【#3 connect DB】

---
+++ db/schema.ts
@@ -0,0 +1,15 @@
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
+
+export const users = pgTable("users_table", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  imageUrl: text("image_url"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+export const insertUsersSchema = createInsertSchema(users);

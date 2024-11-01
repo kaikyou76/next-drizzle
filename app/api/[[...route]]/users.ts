321782d05f422ccb9e2664ea62678ad7851a65b9
kaikyou76 <kaikyou@gmail.com>
2024-11-01 16:00:10 +0800

    【Drizzle ORM】NextJs14 と Drizzle ORM【#１１ Hono Zod Validator 】

--- app/api/[[...route]]/users.ts
+++ app/api/[[...route]]/users.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 import { db } from "@/db/drizzle";
 import { users } from "@/db/schema";
 import { eq } from "drizzle-orm";
@@ -11,10 +13,23 @@ app.get("/", async (c) => {
 });
 
 //idと紐づけるユーザー情報を取得したい場合のロジック
-app.get("/:id", async (c) => {
-  const id = c.req.param("id");
-  const data = await db.select().from(users).where(eq(users.id, id));
-  return c.json({ users: data });
-});
+app.get(
+  "/:id",
+  zValidator(
+    "param",
+    z.object({
+      id: z.string(),
+    })
+  ),
+  async (c) => {
+    //const id = c.req.param("id");
+    const { id } = c.req.valid("param");
+    const [data] = await db.select().from(users).where(eq(users.id, id));
+    if (!data) {
+      return c.json({ error: "user not found!" }, 404);
+    }
+    return c.json({ users: data });
+  }
+);
 
 export default app;

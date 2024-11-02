3840d80411cc67c5617b74c2337bde92ad706301
kaikyou76 <kaikyou@gmail.com>
2024-11-02 08:51:23 +0800

    【Drizzle ORM】NextJs14 と Drizzle ORM【#１３ Drizzle One to One 】

--- db/schema.ts
+++ db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
 
@@ -13,4 +14,18 @@ export const users = pgTable("users_table", {
     .$onUpdate(() => new Date()),
 });
 
+export const usersRelations = relations(users, ({ one, many }) => ({
+  profile: one(profile),
+}));
+
 export const insertUsersSchema = createInsertSchema(users);
+
+export const profile = pgTable("profile_table", {
+  id: text("id").primaryKey(),
+  userId: text("user_id").references(() => users.id, { onDelete: "cascade" }),
+  message: text("message"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .$onUpdate(() => new Date()),
+});

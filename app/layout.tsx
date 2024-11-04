1846abc5cf92fffdd2675bcbde316fe4c75d8237
kaikyou76 <kaikyou@gmail.com>
2024-11-04 14:26:47 +0800

    【Drizzle ORM】NextJs14 と Drizzle ORM【#１８ TanStack Query Provider】

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import "./globals.css";
+import QueryProvider from "@/providers/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,13 +26,15 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
+          <QueryProvider>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            {children}
+          </QueryProvider>
         </body>
       </html>
     </ClerkProvider>

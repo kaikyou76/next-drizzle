5506acd90ed9424f80f304eac0a184d7ba880e5e
kaikyou76 <kaikyou@gmail.com>
2024-11-21 11:29:49 +0800

    【Drizzle ORM】NextJs14 と Drizzle ORM【#３1 Profile Update Route】

--- app/api/[[...route]]/profiles.ts
+++ app/api/[[...route]]/profiles.ts
@@ -2,43 +2,110 @@ import { Hono } from "hono";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { eq } from "drizzle-orm";
-import { db } from "@/db/drizzle";
+import { and, eq } from "drizzle-orm";
 import { createId } from "@paralleldrive/cuid2";
+import { db } from "@/db/drizzle";
 import { profile, users } from "@/db/schema";
 
-const app = new Hono().post(
-  "/",
-  clerkMiddleware(),
-  zValidator("json", z.object({ message: z.string() })),
+const app = new Hono()
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator("json", z.object({ message: z.string() })),
 
-  async (c) => {
-    const auth = getAuth(c);
-    const { message } = c.req.valid("json");
+    async (c) => {
+      const auth = getAuth(c);
+      const { message } = c.req.valid("json");
 
-    if (!auth?.userId) {
-      return c.json({ error: "Unauthorized" }, 401);
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const user = await db.query.users.findFirst({
+        where: eq(users.clerkId, auth.userId),
+      });
+
+      if (!user) {
+        return c.json({ error: "This user not found" }, 404);
+      }
+
+      const existingProfile = await db.query.profile.findFirst({
+        where: eq(profile.userId, user.id),
+      });
+
+      if (existingProfile) {
+        return c.json({ error: "Profile already exist" }, 400);
+      }
+
+      const [data] = await db
+        .insert(profile)
+        .values({
+          id: createId(),
+          userId: user.id,
+          message: message,
+        })
+        .returning();
+
+      return c.json({ data });
     }
+  )
+  .patch(
+    "/:id",
+    clerkMiddleware(),
+    zValidator("param", z.object({ id: z.string() })),
+    zValidator("json", z.object({ id: z.string(), message: z.string() })),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+      const { message } = c.req.valid("json");
 
-    const user = await db.query.users.findFirst({
-      where: eq(users.clerkId, auth.userId),
-    });
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
 
-    if (!user) {
-      return c.json({ error: "This user not found" }, 404);
+      const user = await db.query.users.findFirst({
+        where: eq(users.clerkId, auth.userId),
+      });
+
+      if (!user) {
+        return c.json({ error: "This user not found" }, 404);
+      }
+
+      const [data] = await db
+        .update(profile)
+        .set({ message: message })
+        .where(and(eq(profile.userId, user.id), eq(profile.id, id)))
+        .returning();
+
+      return c.json({ data });
     }
+  )
+  .delete(
+    "/:id",
+    clerkMiddleware(),
+    zValidator("param", z.object({ id: z.string() })),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
 
-    const [data] = await db
-      .insert(profile)
-      .values({
-        id: createId(),
-        userId: user.id,
-        message: message,
-      })
-      .returning();
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
 
-    return c.json({ data });
-  }
-);
+      const user = await db.query.users.findFirst({
+        where: eq(users.clerkId, auth.userId),
+      });
 
+      if (!user) {
+        return c.json({ error: "This user not found" }, 404);
+      }
+
+      const [data] = await db
+        .delete(profile)
+        .where(and(eq(profile.userId, user.id), eq(profile.id, id)))
+        .returning({ id: profile.id });
+
+      return c.json({ data });
+    }
+  );
 export default app;
